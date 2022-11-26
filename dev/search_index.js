50c8aa64a37d15c78eaa81d14d42c6c945270651
Documenter.jl <documenter@juliadocs.github.io>
2022-11-26 10:39:33 +0000

    build based on 61a0e13

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AdventOfCode2022","category":"page"},{"location":"#AdventOfCode2022","page":"Home","title":"AdventOfCode2022","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AdventOfCode2022.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AdventOfCode2022]","category":"page"}]
+}
