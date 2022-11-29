ec28392c04f71a3b0d85deb3496eb5998ececf46
Documenter.jl <documenter@juliadocs.github.io>
2022-11-29 18:08:39 +0000

    build based on cc87aea

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AOC2022","category":"page"},{"location":"#AOC2022","page":"Home","title":"AOC2022","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AOC2022.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AOC2022]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AOC","category":"page"},{"location":"#AOC","page":"Home","title":"AOC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AOC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AOC]","category":"page"}]
 }
